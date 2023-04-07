dc33d621c3b23b12f7c2e5ee3dabbe285bfb7001
Chris Giglio <chrisgiglio@chrisgiglio-macbookpro.local>
2023-04-07 11:55:50 -0500

    First commit

---
+++ todo-app/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onMounted, ref, toRaw } from 'vue';
+import { base64url } from 'multiformats/bases/base64';
+import { PlusIcon as PlusIconMini } from '@heroicons/vue/solid';
+import { CheckCircleIcon, TrashIcon } from '@heroicons/vue/outline';
+
+
+// Handle first load
+onMounted(async () => {
+  
+});
+
+// Add todo to a user's list
+async function addTodo() {
+  
+}
+
+// Set a todo as complete/incomplete
+async function toggleTodoComplete(todoRecordId) {
+  
+}
+
+// Delete a todo from a user's list
+async function deleteTodo(todoRecordId) {
+  
+}
+
+</script>
+
+<template>
+  
+<!-- UI Code goes here -->
+
+</template>

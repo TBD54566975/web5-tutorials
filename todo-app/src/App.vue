8da25e159bb9f9f92ee457b44d1081faf82ae242
bobbilee19 <bobbileehartman@gmail.com>
2023-05-06 17:30:45 -0700

    Matching Todo app guide in docs to new changes and bug fixes

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,34 +1,44 @@
 <script setup>
-import { onMounted, ref, toRaw } from 'vue';
+import { onBeforeMount, onMounted, ref, toRaw } from 'vue';
 import { base64url } from 'multiformats/bases/base64';
 import { PlusIcon as PlusIconMini } from '@heroicons/vue/solid';
 import { CheckCircleIcon, TrashIcon } from '@heroicons/vue/outline';
+import { Web5 } from '@tbd54566975/web5';
 
+// Loading or Creating User Data
+onBeforeMount(async () => {
 
-// Handle first load
-onMounted(async () => {
-  
-});
+    // Populate Todos from DWN
+    const queryResponse = await web5.dwn.records.query(myDid.id, {
 
-// Add todo to a user's list
+// Adding Todos
+const newTodoDescription = ref('');
 async function addTodo() {
-  
-}
 
-// Set a todo as complete/incomplete
-async function toggleTodoComplete(todoRecordId) {
-  
-}
+    // Query Todos
+    const queryResponse = await web5.dwn.records.query(myDid.id, {
 
-// Delete a todo from a user's list
+// Deleting Todos
 async function deleteTodo(todoRecordId) {
-  
-}
+
+// Toggling Todo Status
+async function toggleTodoComplete(todoRecordId) {
 
 </script>
 
 <template>
-  
-<!-- UI Code goes here -->
+    <div class="flex flex-col items-center justify-center min-h-full px-8 py-12 sm:px-6">
+        <!-- Title -->
+        <div class="sm:max-w-md sm:w-full">
+            <h2 class="font-bold text-3xl text-center tracking-tight">
+                Todo List
+            </h2>
+        </div>
+        
+        <!-- Add Todo Form -->
 
+        <!-- Todos -->
+
+                <!-- Delete Todo -->
+    </div>
 </template>
